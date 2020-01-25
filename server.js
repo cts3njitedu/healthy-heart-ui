00a5619468c1425baf0ace609ef50f67b75511dc
Craig Steele <csteele23@Craigs-MacBook-Pro.local>
2020-01-24 19:56:50 -0500

    pushing code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 require('dotenv').config()
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 app.get('/api/*', (req, res) => {
 
@@ -28,26 +28,6 @@ app.get('/api/*', (req, res) => {
    
 })
 
-app.get('/login', (req, res) => {
-
-    let newUrl = req.url.replace(/^(\/api)/, "");
-    console.log(process.env.REACT_APP_HEALTHFUL_HEART_URL);
-    const options = {
-        url: process.env.REACT_APP_HEALTHFUL_HEART_URL + newUrl,
-        method: 'GET',
-        headers: req.headers
-
-    }
-    request(options, function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('headers:', response.headers)
-        
-        res.json(JSON.parse(body))
-    })
-
-   
-})
 
 
 // The "catchall" handler: for any request that doesn't
